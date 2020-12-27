3d1dbdcf532da8ba1ba42d8e91a989f367360998
petruki <31597636+petruki@users.noreply.github.com>
2020-12-27 13:05:13 -0800

    Attempting to fix enconded body

--- src/http/post-graphql/index.js
+++ src/http/post-graphql/index.js
@@ -4,12 +4,7 @@ const { typeDefs } = require('./schema')
 const { resolvers } = require('./resolver')
 
 const server = new ApolloServer({ typeDefs, resolvers })
-const handler = server.createHandler({ 
-  cors: { 
-    origin: '*', 
-    allowedHeaders: ['accept', 'content-type']
-  } 
-})
+const handler = server.createHandler({ cors: { origin: '*' } })
 
 exports.handler = function(event, context, callback) {
   const body = arc.http.helpers.bodyParser(event)
@@ -18,6 +13,8 @@ exports.handler = function(event, context, callback) {
     ? event.httpMethod
     : event.requestContext.http.method
   // Body is now parsed, re-encode to JSON for Apollo
-  event.body = JSON.stringify(body)
+  event.body = Buffer.from(body, 'base64').toString('ascii')
+  event.isBase64Encoded = false;
+
   handler(event, context, callback)
 }
