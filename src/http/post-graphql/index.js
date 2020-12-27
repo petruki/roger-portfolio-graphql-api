020814eeb3ca9da8557cea3c03ff31c7326bb617
petruki <31597636+petruki@users.noreply.github.com>
2020-12-27 02:07:09 -0800

    Attempting to fix cors

--- src/http/post-graphql/index.js
+++ src/http/post-graphql/index.js
@@ -1,12 +1,17 @@
 const arc = require('@architect/functions')
-const { ApolloServer, gql } = require('apollo-server-lambda')
+const { ApolloServer } = require('apollo-server-lambda')
 const { typeDefs } = require('./schema')
 const { resolvers } = require('./resolver')
 
 const server = new ApolloServer({ typeDefs, resolvers })
-const handler = server.createHandler({ cors: { origin: '*' }})
+const handler = server.createHandler()
 
 exports.handler = function(event, context, callback) {
+  const callbackFilter = function(error, output) {
+    output.headers['Access-Control-Allow-Origin'] = '*';
+    callback(error, output);
+  };
+
   const body = arc.http.helpers.bodyParser(event)
   // Support for AWS HTTP API syntax
   event.httpMethod = event.httpMethod
@@ -14,5 +19,5 @@ exports.handler = function(event, context, callback) {
     : event.requestContext.http.method
   // Body is now parsed, re-encode to JSON for Apollo
   event.body = JSON.stringify(body)
-  handler(event, context, callback)
+  handler(event, context, callbackFilter)
 }
